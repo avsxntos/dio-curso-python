8a0552ce7a62f7d38b81724a255e4144ce8fa9b5
avsxntos <thur.vitalino@gmail.com>
2024-06-11 13:08:47 -0300

    Código

---
+++ Variáveis e Constantes.py
@@ -0,0 +1,16 @@
+nome = "Arthur"
+idade = 18
+
+
+nome = "Bruna"
+idade = 21
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+print(limite_saque_diario)
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
