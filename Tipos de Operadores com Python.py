9a1a3fe997e627f37614c711de0aa6fc121b8101
avsxntos <thur.vitalino@gmail.com>
2024-06-11 14:50:25 -0300

    Criação de arquivo Python novo, e adição de conteudo código

--- Tipos de Operadores com Python.py
+++ Tipos de Operadores com Python.py
@@ -1,4 +1,8 @@
-# Adição
+#Adição, Subtração e Multiplicação
+#Operadores Aritméticos
+
+
+ # Adição
 print(1 + 2)
 # 3
 
@@ -10,6 +14,42 @@ print(10 - 2)
 print(4 * 3)
 # 12
 
+# Divisão  e Divisão Inteira
+# Divisão 
+print(12 / 3)
+# 4.0
+
+# Divisão Inteira 
+print(12 // 2)
+# 6
 
 
+# Módulo e exponenciação 
+# Módulo 
+print(10 % 3)
+# 1
+
+# Exponenciação
+print(2 ** 3)
+# 8
+
+
+
+
+# Hierarquia de Cálculos
+
+print(10 - 5 * 2)
+# 0
+
+print((10 - 5) * 2) # o parenteses tendo a ser feito antes, por conta de sua importancia
+# 10
+
+print(10 ** 2 * 2)
+# 200
+
+print(10 ** (2 * 2))
+# 10000
+
+print(10 / 2 * 4)
+# 20.0
 
