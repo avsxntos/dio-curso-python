5bc1b5dd6240c348ae04c20c4e89b30c3fb5c5cb
avsxntos <thur.vitalino@gmail.com>
2024-06-11 13:51:43 -0300

    Criação de Arquivo e Adição de Conteúdo Código

--- Função de Entrada e Saída.py
+++ Função de Entrada e Saída.py
@@ -11,4 +11,16 @@ print(nome, sobrenome, sep="#")
 # Imprime Arthur Santos...
 # Imprime Arthur#Santos
 
+# Aqui é a função de entrada, onde colocamos uma função que nos permite fazer com que o usuário possa inserir algo dado que nós pedirmos
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe sua idade:")
+
+# Após termos criado aquela função, já criamos uma variavél para conter ela, e poder usar para mostrar uma imagem como no código de exemplo abaixo
+print("Entendi! Seu nome então é", nome, "e você tem", idade, "anos!")
+
+
+
+
+
 
