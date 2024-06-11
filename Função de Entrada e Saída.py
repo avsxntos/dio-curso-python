572ba575f36c34af13adf5e8cec563e8691be076
avsxntos <thur.vitalino@gmail.com>
2024-06-11 13:31:47 -0300

    Criação de um arquivo novo e adição de conteúdo código

---
+++ Função de Entrada e Saída.py
@@ -0,0 +1,14 @@
+# Aqui podemos ver um pouco sobre funções de entrada saída, e a seguir terão alguns exemplos
+
+nome = "Arthur"
+sobrenome = "Santos"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+
+# imprime Arthur Santos
+# Imprime Arthur Santos...
+# Imprime Arthur#Santos
+
+
