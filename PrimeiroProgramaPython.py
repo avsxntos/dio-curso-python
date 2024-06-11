717a8c18790d4d8faf8e6b664901983707ef40b9
avsxntos <thur.vitalino@gmail.com>
2024-06-11 12:43:07 -0300

    Teste de tipos de códigos e Testes de Lógica com Python

--- PrimeiroProgramaPython.py
+++ PrimeiroProgramaPython.py
@@ -1,1 +1,15 @@
-print("Olá, seja bem vindo ao curso de Python!")
+# Primeiro código feito!
+# print("Olá, seja bem vindo ao curso de Python!")
+
+# Teste de lógica e tipos de códigos
+# print(11 + 10 + 90281390183)
+
+#agora testando com número de ponto flutuante (float)
+print(1.5 + 200)
+
+#agora o tipo booleano (true or false)
+print(True)
+print(False)
+
+# E por fim o tipo String
+print("Python code")
