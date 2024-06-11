50d8bac58d8e29ef9c09bdf81299ba71b820ace7
avsxntos <thur.vitalino@gmail.com>
2024-06-11 13:22:20 -0300

    Adição de conteúdo código no arquivo de Conversão de Tipos

---
+++ Coversão de Tipos.py
@@ -0,0 +1,57 @@
+# Primeiro exemplo, mudando valor inteiro para um valor flutuante (flaot)
+
+preco = 10
+print(preco)
+# imprime 10
+
+preco = float(preco)
+print(preco)
+# imprime 10.0
+
+preco = 10 / 2
+print(preco)
+# imprime 5.0
+
+
+
+
+
+
+# agora mudando de float para inteiro
+preco = 10.30
+print(preco)
+ # imprime 10.3
+
+preco = int(preco)
+print(preco)
+# imprime 10
+
+preco = 10
+print(preco)
+# imprime 10
+
+print (preco / 2)
+# imprime 5.0
+
+print (preco // 2) # aqui neste caso onde vemos duas barras, por ser duas, ele acaba preservando o resultado para o número inteiro!
+# imprime 5
+
+
+
+
+
+# Exemplo de Númerico para String
+preco = 10.50
+idade = 28
+
+print(str(preco))
+# imprime 10.5
+
+print(str(idade))
+# imprime 28
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+# imprime idade 28 preco 10.5
+
+
